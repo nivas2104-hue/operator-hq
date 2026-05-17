32e54e17840eccdefc8e628e304eb32eea2fe722
nivas2104-hue <nivas2104@gmail.com>
2026-05-17 10:48:53 +0530

    Connect Notion through Netlify function

--- netlify/functions/notion.js
+++ netlify/functions/notion.js
@@ -1,28 +1,35 @@
 exports.handler = async (event) => {
   try {
-    const { token } = JSON.parse(event.body);
+    const { token, databaseId } = JSON.parse(event.body);
 
-    const response = await fetch("https://api.notion.com/v1/users/me", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Notion-Version": "2022-06-28"
+    const response = await fetch(
+      `https://api.notion.com/v1/databases/${databaseId}/query`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Notion-Version': '2022-06-28',
+          'Content-Type': 'application/json'
+        }
       }
-    });
+    );
 
     const data = await response.json();
 
     return {
-      statusCode: response.status,
+      statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "*"
+        'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify(data)
     };
+
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: err.message })
+      body: JSON.stringify({
+        error: err.message
+      })
     };
   }
 };
